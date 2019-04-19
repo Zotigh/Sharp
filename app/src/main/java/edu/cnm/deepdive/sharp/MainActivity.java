99a3acf7760c60532fc9ef63400011ba7e8dd8b6
Lance Zotigh <lancezotighlz@gmail.com>
2019-04-18 22:37:11 -0600

    working on fragments

--- app/src/main/java/edu/cnm/deepdive/sharp/MainActivity.java
+++ app/src/main/java/edu/cnm/deepdive/sharp/MainActivity.java
@@ -3,42 +3,43 @@ package edu.cnm.deepdive.sharp;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-  private TextView mTextMessage;
-
-  private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-      = new BottomNavigationView.OnNavigationItemSelectedListener() {
-
-    @Override
-    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-      switch (item.getItemId()) {
-        case R.id.navigation_home:
-          mTextMessage.setText(R.string.title_home);
-          return true;
-        case R.id.navigation_dashboard:
-          mTextMessage.setText(R.string.title_dashboard);
-          return true;
-        case R.id.navigation_notifications:
-          mTextMessage.setText(R.string.title_notifications);
-          return true;
-      }
-      return false;
-    }
-  };
-
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
 
-    mTextMessage = (TextView) findViewById(R.id.message);
-    BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-    navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
-  }
+    BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+    bottomNav.setOnNavigationItemSelectedListener(navListener);
 
+    if (savedInstanceState == null) {
+      getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+          new RandomizeFragment()).commit();
+    }
+  }
+  private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+      item -> {
+        Fragment selectedFragment = null;
+
+        switch (item.getItemId()) {
+          case R.id.navigation_randomize:
+            selectedFragment = new RandomizeFragment();
+            break;
+          case R.id.navigation_recipes:
+            selectedFragment = new SavedRecipesFragment();
+            break;
+        }
+
+        assert selectedFragment != null;
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+            selectedFragment).commit();
+
+        return true;
+      };
 }
