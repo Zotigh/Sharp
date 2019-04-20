9b3dee5a455a5ad2df7984864361f63bff5eba1b
Lance Zotigh <lancezotighlz@gmail.com>
2019-04-20 09:40:35 -0600

    finishing up frag

--- app/src/main/java/edu/cnm/deepdive/sharp/MainActivity.java
+++ app/src/main/java/edu/cnm/deepdive/sharp/MainActivity.java
@@ -10,36 +10,59 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-    BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
-    bottomNav.setOnNavigationItemSelectedListener(navListener);
+        BottomNavigationView bottomNav = findViewById(R.id.navigation);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
 
-    if (savedInstanceState == null) {
-      getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
-          new RandomizeFragment()).commit();
-    }
-  }
-  private BottomNavigationView.OnNavigationItemSelectedListener navListener =
-      item -> {
-        Fragment selectedFragment = null;
-
-        switch (item.getItemId()) {
-          case R.id.navigation_randomize:
-            selectedFragment = new RandomizeFragment();
-            break;
-          case R.id.navigation_recipes:
-            selectedFragment = new SavedRecipesFragment();
-            break;
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                new TempFragment()).commit();
         }
+    }
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+        item -> {
+            Fragment selectedFragment = null;
 
-        assert selectedFragment != null;
-        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
-            selectedFragment).commit();
+            switch (item.getItemId()) {
+                case R.id.fragment_container:
+                    selectedFragment = new TempFragment();
+                    break;
+            }
 
-        return true;
-      };
+            assert selectedFragment != null;
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                selectedFragment).commit();
+
+            return true;
+        };
+
+//
+//
+//
+//    private TextView mTextMessage;
+//
+//    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+//            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+//
+//        @Override
+//        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+//            switch (item.getItemId()) {
+//                case R.id.navigation_home:
+//                    mTextMessage.setText(R.string.title_home);
+//                    return true;
+//                case R.id.navigation_dashboard:
+//                    mTextMessage.setText(R.string.title_dashboard);
+//                    return true;
+//                case R.id.navigation_notifications:
+//                    mTextMessage.setText(R.string.title_notifications);
+//                    return true;
+//            }
+//            return false;
+//        }
+//    };
+
 }
