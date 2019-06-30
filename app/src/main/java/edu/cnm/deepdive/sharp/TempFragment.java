05b7da121c813006da006084b265fcb7eb8039aa
Lance Zotigh <lancezotighlz@gmail.com>
2019-06-30 13:03:14 -0600

    getting spinner to work in a fragment I

--- app/src/main/java/edu/cnm/deepdive/sharp/TempFragment.java
+++ app/src/main/java/edu/cnm/deepdive/sharp/TempFragment.java
@@ -5,16 +5,40 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
+import java.util.Objects;
 
 
 public class TempFragment extends Fragment {
 
-  Textview textview;
+  private Spinner spinner;
+  ArrayAdapter<CharSequence> adapter;
+
   @Override
   public View onCreateView(LayoutInflater inflater, ViewGroup container,
       Bundle savedInstanceState) {
-    return inflater.inflate(R.layout.fragment_temp, container, false);
+    View view = inflater.inflate(R.layout.fragment_temp, container, false);
 
+    spinner = view.findViewById(R.id.spinnerI);
+
+    adapter = ArrayAdapter.createFromResource(Objects.requireNonNull(getActivity()), R.array.listI, android.R.layout.simple_spinner_item );
+    adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+    spinner.setAdapter(adapter);
+    spinner.setOnItemSelectedListener();
+    return view;
   }
 
+  @Override
+  public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+    String text = parent.getItemAtPosition(position).toString();
+    Toast.makeText(parent.getContext(), text, Toast.LENGTH_SHORT).show();
+  }
+
+  @Override
+  public void onNothingSelected(AdapterView<?> parent) {
+
+  }
 }
